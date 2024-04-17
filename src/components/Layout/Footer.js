8327c972af96a038d7b3f5bbd98546eac91c6980
silent-code007 <rajivmandal341@gmail.com>
2024-04-17 23:53:21 +0530

    ecommerce-frontend

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h1 className="text-center">All Right Reserved &copy; Ecommerce</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/pages/PagenotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Layout } from '../components/Layout/Layout'
+
+const PagenotFound = () => {
+  return (
+   <Layout>
+    <h1>
+        PagenotFound
+    </h1>
+   </Layout>
+  )
+}
+
+export default PagenotFound
