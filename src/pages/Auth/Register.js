1ce5b97077aa0f2c56eaee42bd2c6455a857dfd8
silent-code007 <rajivmandal341@gmail.com>
2024-04-21 10:26:25 +0530

    New Commit

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -1,25 +1,32 @@
-import { React, useState } from "react";
-import { Layout } from "../../components/Layout/Layout";
-import { toast } from "react-toastify";
+import React, { useState } from "react";
+import {Layout} from "./../../components/Layout/Layout";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import "../../styles/AuthStyles.css";
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [address, setAddress] = useState("");
+  const [answer, setAnswer] = useState("");
   const navigate = useNavigate();
+
   // form function
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(
-        `${process.env.REACT_APP_API}/api/v1/auth/register`,
-        { name, email, password, phone, address }
-      );
-      if (res.data.success) {
-        toast.success(res.data.message);
+      const res = await axios.post("/api/v1/auth/register", {
+        name,
+        email,
+        password,
+        phone,
+        address,
+        answer,
+      });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
         navigate("/login");
       } else {
         toast.error(res.data.message);
@@ -31,10 +38,10 @@ const Register = () => {
   };
 
   return (
-    <Layout title={"Register - Ecommerce App"}>
-      <div className="register">
-        <h1>Register Page</h1>
+    <Layout title="Register - Ecommer App">
+      <div className="form-container" style={{ minHeight: "90vh" }}>
         <form onSubmit={handleSubmit}>
+          <h4 className="title">REGISTER FORM</h4>
           <div className="mb-3">
             <input
               type="text"
@@ -42,65 +49,68 @@ const Register = () => {
               onChange={(e) => setName(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
-              placeholder="Name"
+              placeholder="Enter Your Name"
               required
+              autoFocus
             />
           </div>
-
-          {/* email */}
-
           <div className="mb-3">
             <input
-              type="text"
+              type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
-              placeholder="Email"
+              placeholder="Enter Your Email "
               required
             />
           </div>
-          {/* password  */}
-
           <div className="mb-3">
             <input
-              type="text"
+              type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="form-control"
-              id="exampleInputPassword"
-              placeholder="**************"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Password"
               required
             />
           </div>
-          {/* phone */}
-
           <div className="mb-3">
             <input
               type="text"
               value={phone}
               onChange={(e) => setPhone(e.target.value)}
               className="form-control"
-              id="exampleInputPhone"
-              placeholder="Phone"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Phone"
               required
             />
           </div>
-          {/* Address */}
-
           <div className="mb-3">
             <input
               type="text"
               value={address}
               onChange={(e) => setAddress(e.target.value)}
               className="form-control"
-              id="exampleInputAddress"
-              placeholder="Address"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Address"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="What is Your Favorite sports"
               required
             />
           </div>
           <button type="submit" className="btn btn-primary">
-            Submit
+            REGISTER
           </button>
         </form>
       </div>
