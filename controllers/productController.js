11fae2b8ebc292b4bcc3c23c5388a7cd7a0479d4
silent-code007 <rajivmandal341@gmail.com>
2024-05-06 00:17:52 +0530

    New Commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,21 @@
 import productModel from "../models/productModel.js";
 import categoryModel from "../models/categoryModel.js";
-import orderModel from "../models/orderModel.js";
+// import orderModel from "../models/orderModel.js";
 
 import fs from "fs";
 import slugify from "slugify";
-import braintree from "braintree";
+// import braintree from "braintree";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 //payment gateway
-var gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
-});
+// var gateway = new braintree.BraintreeGateway({
+  // environment: braintree.Environment.Sandbox,
+//   merchantId: process.env.BRAINTREE_MERCHANT_ID,
+//   publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+//   privateKey: process.env.BRAINTREE_PRIVATE_KEY, 
+// });
 
 export const createProductController = async (req, res) => {
   try {
